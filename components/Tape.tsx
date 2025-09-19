9034b377b5f7237a9e4ba2858a0afc0ff31cc940
Mohand Amged <mohandamged70m@gmail.com>
2025-09-19 10:23:14 -0700

    fix download link

--- components/Tape.tsx
+++ components/Tape.tsx
@@ -4,7 +4,7 @@ import { TbNorthStar } from "react-icons/tb";
 const Tape = () => {
   return (
     <div className="sm:py-10 md:py-16">
-      <a  href="/Mohand amged CV.pdf" download>
+      <a  href="/Mohand Amged CV.pdf" download>
         <div className="bg-gradient-to-r via-purple-500 from-pink-500 to-blue-500 overflow-hidden -rotate-3 -mx-1 py-2">
           <div className="relative flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
             {/* Primary scrolling content */}
